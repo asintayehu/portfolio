523d74732c9e25e9c0e127dd16481fa51fe189c3
Aaron <asintayehu@proton.me>
2023-01-10 11:44:15 -0500

    extremely minor changes, added some text thats all

---
+++ src/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './header.css'
+
+const Header = () => {
+  return (
+    <>
+      <div className="container header__container">
+        <h1>Aaron Sintayehu</h1>
+        <h2>Web Developer</h2>
+      </div>
+
+    </>
+  )
+}
+
+export default Header
